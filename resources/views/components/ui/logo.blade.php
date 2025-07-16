56ac0c4fb865c587f8b3ccd7e8953c5ecf5ee2d1
kemal <kemalyen@gmail.com>
2025-07-16 14:19:17 +0100

    ui fix"

--- resources/views/components/ui/logo.blade.php
+++ resources/views/components/ui/logo.blade.php
@@ -1,6 +1,6 @@
  
 
-<div class="ms-1 grid flex-1 text-start text-sm">
+<div class="ms-1 grid flex-1 text-center text-sm">
     
     <span class="mb-0.5 truncate leading-tight font-semibold text-2xl text-red-600">{{ config('app.name') }}</span>
 </div>
